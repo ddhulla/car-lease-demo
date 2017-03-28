33e71d80725a693d2659c1747156507cc9c8e8f3
ddhulla <dharmen.dhulla@gmail.com>
2017-03-27 23:34:55 -0700

    Modified Asset Create js

--- Client_Side/JavaScript/asset_functions/asset_interaction.js
+++ Client_Side/JavaScript/asset_functions/asset_interaction.js
@@ -18,8 +18,7 @@ function createAsset()
     xhr.onprogress = function () {
         let data = xhr.responseText;
         let array = data.split('&&');
-		alert(data);
-		alert("After Data");
+		alert('After Data');
         for(let i = 0; i < array.length; i++)
 		{
             if(array[i] != '')
